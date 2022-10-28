e4e5171519aca49addc7216f1cade2787d8e60a1
Xenko10 <101007014+Xenko10@users.noreply.github.com>
2022-10-28 21:16:53 +0200

    prostokat

--- cpp_code/Code/project/main.cpp
+++ cpp_code/Code/project/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
     char koniec, znak;
+    int wybor;
     do {
 
     cout << "Podaj znak: ";
@@ -21,11 +22,41 @@ int main()
     cout << "Podaj wysokosc:";
     cin >> wysokosc;
 
+    cout << "Wypelniony prostakat - 1 czy pusty - 2: ";
+
+    cin >> wybor;
+
+    switch (wybor){
+
+    case 1:
+
     for (int i=0; i<dlugosc; i++){
         for (int j=0; j<wysokosc; j++){
             cout << znak;
         }
         cout << endl;
+    }
+
+    break;
+
+    case 2:
+
+    for (int i=0; i<dlugosc; i++){
+        for (int j=0; j<wysokosc; j++){
+            if (j==0 || i==0 || i==dlugosc-1 || j==wysokosc-1){
+            cout << znak;
+            }
+        else{
+            cout << " ";
+        }
+        }
+        cout << endl;
+    }
+
+    break;
+
+    default:
+        cout << "Zly wybor.\n\n";
     }
 
     cout << "Czy chcesz zakonczyc program? \n";
