0c579dbf7bc3b36f256f146a48b55b86ffd360ee
Xenko10 <101007014+Xenko10@users.noreply.github.com>
2022-11-16 22:26:31 +0100

    podciag proba

--- podciag/main.cpp
+++ podciag/main.cpp
@@ -10,7 +10,7 @@ int main()
     const int rozmiar = 51;
     char ciag[rozmiar], podciag[rozmiar], wynik[rozmiar];
 
-    cout << "Wylosowany ciag: ";
+    cout << "Wylosowany ciag: \n";
 
     for (int i = 0; i<rozmiar-1; i++){
         ciag[i] = (char) (97+rand() % 26);
@@ -22,11 +22,50 @@ int main()
     cin >> podciag;
 
 
+    int podciagDlugosc = 0;
 
-    for (int i=0; i<rozmiar-1; i++){
-        if (ciag[i]==podciag[0]){
-
+    for (int i = 0; i<rozmiar-1; i++){
+        if (podciag[i] != NULL){
+            podciagDlugosc++;
         }
     }
 
+    if (podciagDlugosc>3){
+    int poczatek = 0;
+    int iloscSpacji = 0;
+
+    for (int i=0; i<rozmiar-1; i++){
+        if (ciag[i]==podciag[0]){
+                poczatek = i;
+            for (int j=0; j<podciagDlugosc; j++){
+                if (ciag[i+j] == podciag[j] && ciag[i+j] != NULL){
+                    wynik[j] = ciag[i+j];
+                }
+                else{
+                    break;
+                }
+            }
+
+        }
+        else {
+            iloscSpacji++;
+        }
+
+    }
+
+    for (int i=0; i<poczatek; i++){
+        cout << " ";
+    }
+    cout << podciag << endl;
+
+    if (wynik == podciag){
+    cout << "Jest taki ciag: " << podciag << ". Zaczyna sie na: " << poczatek << " miejscu.";
+    }
+    else{
+        cout << "Nie ma takiego podciagu. ";
+    }
+    }
+    else{
+        cout << "Podales za maly ciag";
+    }
 }
