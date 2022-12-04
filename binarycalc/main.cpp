b94b0ba8d51042d65c9b613be695b6298a922bfd
Xenko10 <101007014+Xenko10@users.noreply.github.com>
2022-12-04 18:23:03 +0100

    Update main.cpp

--- binarycalc/main.cpp
+++ binarycalc/main.cpp
@@ -22,11 +22,49 @@ void dec_to_bin(long int n){
     }
 }
 
+void dec_to_hex(long int n){
+    const int rozmiar = 50;
+    int temp, i=1,j , r;
+    char wynik[rozmiar];
+    temp = n;
+    while (temp != 0){
+        r = temp % 16;
+        if (r < 10){
+            wynik[i++] = r + 48;
+        }
+        else{
+            wynik[i++] = r + 55;
+        }
+        temp = temp / 16;
+    }
+    cout << "Heksadecymalna liczba " << n << " jest:";
+    for (j=i; j > 0; j--){
+        cout << wynik[j];
+    }
+}
+
 int main()
 {
     long int n;
-    cout << "Podaj liczbe dziesietna do zmienienia na binarna: ";
-    cin >> n;
-    dec_to_bin(n);
+    int wybor;
+
+    cout << "Wybierz konwersje: " << endl;
+    cout << "1. dec->bin, 2. dec->hex" << endl;
+    cin >> wybor;
+
+    switch(wybor){
+
+    case 1:
+        cout << "Podaj liczbe dziesietna do zmienienia na binarna: ";
+        cin >> n;
+        dec_to_bin(n);
+    break;
+
+    case 2:
+        cout << "Podaj liczbe dziesietna do zmienienia na heksadecymalna: ";
+        cin >> n;
+        dec_to_hex(n);
+    break;
+    }
     return 0;
 }
